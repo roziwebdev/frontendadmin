0e07e02d781dbbf46a0d0434a3f7974c9c561448
rozi <roziwebdev@gmail.com>
2024-09-16 13:44:17 +0700

    change route user

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,46 +1,27 @@
-import { authOptions } from "@/libs/AuthOption";
-import { getServerSession } from "next-auth";
+import prismadb from "@/libs/prismadb";
 import { NextResponse } from "next/server";
-
-export async function GET(req: Request) {
-    try {
-        const session = await getServerSession(authOptions);
-        if (!session?.user) {
-            return new NextResponse('Unauthorized', { status: 401 });
-        }
-        const { search = "", page = "1", limit = "10" } = Object.fromEntries(new URL(req.url).searchParams);
+export async function GET(request: Request) {
+    const { search = "", page = "1", limit = "10" } = Object.fromEntries(new URL(request.url).searchParams);
         const pageNumber = parseInt(page, 10);
         const pageSize = parseInt(limit, 10);
-        const userRole = session.user.role;
-
         let whereClause: any = {
             name: {
                 contains: search,
-            }
-        }
+            },
+        };
+        const users = await prismadb.user.findMany({
+            where: whereClause,
+            skip: (pageNumber - 1) * pageSize,
+            take: pageSize,
+        });
 
-        if (userRole !== "Admin") {
-            return new NextResponse('Unauthorized', { status: 401 });
-        } else if (userRole === "Admin") {
-            const totalCount = await prisma.user.count({ where: whereClause });
-            const users = await prisma.user.findMany({
-                where: whereClause,
-                skip: (pageNumber - 1) * pageSize,
-                take: pageSize,
-            });
-            const totalPages = Math.ceil(totalCount / pageSize);
+        const totalCount = await prismadb.user.count({ where: whereClause });
+        const totalPages = Math.ceil(totalCount / pageSize);
 
-            return NextResponse.json({
-                users,
-                currentPage: pageNumber,
-                totalPages,
-                totalCount
-            });
-        }
-
-    } catch (error) {
-        console.log(error);
-        return new NextResponse('Internal Error', { status: 500 });
-    }
+        return NextResponse.json({
+            users,
+            currentPage: pageNumber,
+            totalPages,
+            totalCount,
+        });
 }
-
